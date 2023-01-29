e8062b78ef7a347304774f6179b04af65e6cb35c
itx-shoaib <shoaibjamil43@gmail.com>
2023-01-29 05:24:49 +0500

    Adding react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        hello
+      </h1>
+    </>
+    //   <Routes>
+    //   <Route path='/' element={<Signin />} />
+    // </Routes>
+  );
+}
+
+export default App;
