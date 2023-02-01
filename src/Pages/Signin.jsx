4cb3dbef64d4c1d0420c1fcdd914b0d974a30ab5
itx-shoaib <shoaibjamil43@gmail.com>
2023-02-02 04:52:05 +0500

    Updating signin page

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -10,90 +10,90 @@ function Signin() {
     };
     return (
         <div>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="row m-5">
-                            <div className="col-md-2"></div>
+            <section className="h-100">
+                <div className="container containerSignin py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col">
+                            <div className="card card-registration my-4">
+                                <div className="row g-0">
+                                    <div className="col-xl-12">
+                                        <div className="card-body p-md-5 m-5 text-black">
+                                            <h1 className="text-center">Authentication</h1>
+                                            <Form
+                                                name="basic"
+                                                labelCol={{
+                                                    span: 8,
+                                                }}
+                                                wrapperCol={{
+                                                    span: 16,
+                                                }}
+                                                style={{
+                                                    maxWidth: 600,
+                                                }}
+                                                initialValues={{
+                                                    remember: true,
+                                                }}
+                                                onFinish={onFinish}
+                                                onFinishFailed={onFinishFailed}
+                                                autoComplete="off"
+                                            >
+                                                <Form.Item
+                                                    label="Email"
+                                                    name="Email"
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your Email!',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input type="email" />
+                                                </Form.Item>
 
-                            <div className="col-md-6 border border-secondary p-2">
-                                <h1 className="text-center">Authentication</h1>
-                                <Form
-                                    name="basic"
-                                    labelCol={{
-                                        span: 8,
-                                    }}
-                                    wrapperCol={{
-                                        span: 16,
-                                    }}
-                                    style={{
-                                        maxWidth: 600,
-                                    }}
-                                    initialValues={{
-                                        remember: true,
-                                    }}
-                                    onFinish={onFinish}
-                                    onFinishFailed={onFinishFailed}
-                                    autoComplete="off"
-                                >
-                                    <Form.Item
-                                        label="Email"
-                                        name="Email"
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: 'Please input your Email!',
-                                            },
-                                        ]}
-                                    >
-                                        <Input type="email" />
-                                    </Form.Item>
+                                                <Form.Item
+                                                    label="Password"
+                                                    name="password"
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your password!',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input.Password />
+                                                </Form.Item>
 
-                                    <Form.Item
-                                        label="Password"
-                                        name="password"
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: 'Please input your password!',
-                                            },
-                                        ]}
-                                    >
-                                        <Input.Password />
-                                    </Form.Item>
+                                                <Form.Item
+                                                    name="remember"
+                                                    valuePropName="checked"
+                                                    wrapperCol={{
+                                                        offset: 8,
+                                                        span: 16,
+                                                    }}
+                                                >
+                                                    <Checkbox>Remember me</Checkbox>
+                                                </Form.Item>
 
-                                    <Form.Item
-                                        name="remember"
-                                        valuePropName="checked"
-                                        wrapperCol={{
-                                            offset: 8,
-                                            span: 16,
-                                        }}
-                                    >
-                                        <Checkbox>Remember me</Checkbox>
-                                    </Form.Item>
-
-                                    <Form.Item
-                                        className='text-center'
-                                        wrapperCol={{
-                                            offset: 8,
-                                            span: 16,
-                                        }}
-                                    >
-                                        <Button type="primary" htmlType="submit">
-                                            Submit
-                                        </Button>
-                                    </Form.Item>
-                                </Form>
+                                                <Form.Item
+                                                    className='text-center'
+                                                    wrapperCol={{
+                                                        offset: 8,
+                                                        span: 16,
+                                                    }}
+                                                >
+                                                    <Button type="primary" htmlType="submit">
+                                                        Submit
+                                                    </Button>
+                                                </Form.Item>
+                                            </Form>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="col-md-2"></div>
                         </div>
                     </div>
                 </div>
-
-            </div>
-
-
+            </section>
         </div>
     )
 }
